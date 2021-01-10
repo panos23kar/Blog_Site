ae0fe06ef7a2060fa45cebff3649736d2f0ad87b
panos23kar <panos23kar@gmail.com>
2021-01-10 19:14:27 +0100

    wire PostListView with a url

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('about/', views.AboutView.as_view(),name='about')
+    path('',views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(),name='about'),
 ]
