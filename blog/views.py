c301ee4c77f8db3f4179bd7326d0ecd491096fdb
panos23kar <panos23kar@gmail.com>
2021-01-10 18:38:27 +0100

    create AboutView and base.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import (TemplateView)#parenthesis to extend to multiple lines
 # Create your views here.
+
+class AboutView(TemplateView):
+    #TODO na brw ti einai kai apo pou erxetai to template_name
+    template_name = 'about.html'
