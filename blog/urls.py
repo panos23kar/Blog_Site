7f7c961296ce62749c2c2ace42b194438de350b3
panos23kar <panos23kar@gmail.com>
2021-01-10 19:21:23 +0100

    wire PostDetailView with url

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path('',views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(),name='about'),
+    path(r'post/(?P<pk>\d+)$', views.PostDetailView.as_view(),name='post_detail') #documentaion
 ]
