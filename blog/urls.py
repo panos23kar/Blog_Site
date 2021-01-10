469199b6a4ec27b8982604ce5c562424d8d12baf
panos23kar <panos23kar@gmail.com>
2021-01-10 19:37:18 +0100

    wire CreatePostView with urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(),name='about'),
     # Documentation: Maybe I need to change it with slug somehow https://docs.djangoproject.com/en/3.1/ref/class-based-views/generic-display/ 
-    path(r'post/(?P<pk>\d+)$', views.PostDetailView.as_view(),name='post_detail') #documentaion
+    path(r'post/(?P<pk>\d+)$', views.PostDetailView.as_view(),name='post_detail'),
+    path('post/new', views.CreatePostView.as_view(),name='post_new'),
 ]
