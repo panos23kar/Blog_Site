abb3dfabf7046c118cb7f959594d72b784608914
panos23kar <panos23kar@gmail.com>
2021-01-10 20:16:43 +0100

    wire PostDeleteView with urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # Documentation: Maybe I need to change it with slug somehow https://docs.djangoproject.com/en/3.1/ref/class-based-views/generic-display/ 
     path(r'post/(?P<pk>\d+)$', views.PostDetailView.as_view(),name='post_detail'),
     path('post/new', views.CreatePostView.as_view(),name='post_new'),
-    path(r'post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(),name='post_edit')
+    path(r'post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(),name='post_edit'),
+    path(r'post/(?P<pk>\d+)/remove/$', views.PostDeleteView.as_view(),name='post_remove')
 ]
