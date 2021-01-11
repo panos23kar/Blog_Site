1a6cb4fe63a71811d77c3208bf8ab4ff6ca69512
panos23kar <panos23kar@gmail.com>
2021-01-11 17:32:25 +0100

    create comment_approve function

--- blog/views.py
+++ blog/views.py
@@ -89,3 +89,9 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/comment_form.html', {'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
