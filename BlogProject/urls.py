9870161cf29c5e6da99a64194d539df5503dc55a
panos23kar <panos23kar@gmail.com>
2021-01-11 18:25:03 +0100

    Set up an authentication system

--- BlogProject/urls.py
+++ BlogProject/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blogs.urls')),
+    path('account/login/', views.LoginView.as_view(), name='login'),
+    # when logout you go to home page
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page':'/'})
     
 ]
