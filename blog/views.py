735660ab33d0fd41cb239bef36ccd87dea16553e
panos23kar <panos23kar@gmail.com>
2021-01-11 17:39:52 +0100

    create comment_remove

--- blog/views.py
+++ blog/views.py
@@ -95,3 +95,10 @@ def comment_approve(request,pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
