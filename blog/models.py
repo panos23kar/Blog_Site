e98a79365d698ef14f6122a80a26fe80b00d87b5
panos23kar <panos23kar@gmail.com>
2021-01-16 23:03:02 +0100

    typo in models

--- blog/models.py
+++ blog/models.py
@@ -34,7 +34,7 @@ class Comment(models.Model):
     post                = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author              = models.CharField(max_length=200)
     text                = models.TextField()
-    create_date         = models.DateTimeField(default=timezone.now())
+    created_date         = models.DateTimeField(default=timezone.now())
     approved_comment    = models.BooleanField(default=False)
 
     def approve(self):
