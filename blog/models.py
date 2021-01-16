24db86e16dd300d5d8ffcf36aa60d5586cfb4f6f
panos23kar <panos23kar@gmail.com>
2021-01-16 22:09:48 +0100

    fix foreign keys of models on_delete

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 # Create your models here.
 
 class Post(models.Model):
-    author          = models.ForeignKey('auth.User')
+    author          = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title           = models.CharField(max_length=200)
     text            = models.TextField()
     create_date     = models.DateTimeField(default=timezone.now())
@@ -31,7 +31,7 @@ class Post(models.Model):
         return self.title
 
 class Comment(model.Model):
-    post                = models.ForeignKey('blog.Post', related_name='comments')
+    post                = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author              = models.CharField(max_length=200)
     text                = models.TextField()
     create_date         = models.DateTimeField(default=timezone.now())
